006219f809464c3cb7807b80e5633ac7252879f8
Mirus <mirusim@gmail.com>
2022-10-18 10:57:24 +0300

    chore: add github link

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -8,6 +8,7 @@ pub fn Footer<T: Html>(cx: Scope) -> View<T> {
         p {"Try to build a WASM app with "
         a(href="https://github.com/sycamore-rs/sycamore", class="footer__link") {"sycamore"}
         " by yourself"}
+        p {"Check it out on " a(class="footer__link", href="https://github.com/Miruzz/wassword-generator") {"GitHub"}}
       }
     }
 }
